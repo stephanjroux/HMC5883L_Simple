c98646d2d69339946fe95352cdfe9da7a81d806b
stephanjroux <stephanjroux@gmail.com>
2015-12-27 16:00:18 +0200

    Update hmc588l_simple.cpp

--- firmware/hmc588l_simple.cpp
+++ firmware/hmc588l_simple.cpp
@@ -37,7 +37,7 @@
 // you must include Wire.h in your main sketch, the Arduino IDE will not include Wire
 // in the build process otherwise.
 
-//#include "HMC5883L_Simple/HMC5883L_Simple.h"
+#include "hmc5883l_simple.h"
 
 HMC5883L_Simple::HMC5883L_Simple()
 {
